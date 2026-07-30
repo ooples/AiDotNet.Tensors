682592a2a1281d382a079517b126c607235f9fb8
ooples <cheatcountry@gmail.com>
2026-07-30 17:50:00 -0400

    fix(bench): forward dynamic shared launch bytes

--- tests/AiDotNet.Tensors.Benchmarks/DirectPtxLaunchHelper.cs
+++ tests/AiDotNet.Tensors.Benchmarks/DirectPtxLaunchHelper.cs
@@ -10,13 +10,16 @@ internal static class DirectPtxLaunchHelper
 {
     internal static unsafe void Launch(
         DirectPtxModule module, IntPtr function, IntPtr[] pointers,
-        uint blocks, uint threads)
+        uint blocks, uint threads, uint dynamicSharedMemoryBytes = 0)
     {
+        if (module is null) throw new ArgumentNullException(nameof(module));
+        if (pointers is null) throw new ArgumentNullException(nameof(pointers));
         fixed (IntPtr* pinned = pointers)
         {
             void** arguments = stackalloc void*[pointers.Length];
             for (int i = 0; i < pointers.Length; i++) arguments[i] = pinned + i;
-            module.Launch(function, blocks, 1, 1, threads, 1, 1, 0, arguments);
+            module.Launch(function, blocks, 1, 1, threads, 1, 1,
+                dynamicSharedMemoryBytes, arguments);
         }
     }
 }
